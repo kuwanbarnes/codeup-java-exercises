732e182cfe51e7c199cd4285c4d9ea4ee10aac7a
kuwan barnes <Kuwan.barne@gmail.com>
2021-01-25 12:34:46 -0600

    finished pb 2

---
+++ src/Bob.java
@@ -0,0 +1,19 @@
+import java.util.Scanner;
+
+public class Bob {
+    public static void main(String[] args) {
+        Scanner sc= new Scanner(System.in);
+        System.out.println("what would you like to say to Bob?");
+        String response= sc.nextLine();
+        if (response.endsWith("!")){
+            System.out.println("Woah, chill out!");
+        }else if (response.endsWith("?")){
+            System.out.println("Sure");
+        }else if (response.length()< 1){
+            System.out.println("fine, be that way.");
+        }else{
+            System.out.println("Whatever.");
+        }
+
+    }
+}
