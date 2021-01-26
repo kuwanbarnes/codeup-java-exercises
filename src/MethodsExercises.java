bc4edb65c18ac06de9d082211b6996d40d4b24e7
kuwan barnes <Kuwan.barne@gmail.com>
2021-01-26 12:11:10 -0600

    added modulus

--- src/MethodsExercises.java
+++ src/MethodsExercises.java
@@ -23,10 +23,17 @@ public class MethodsExercises {
         return num1 / num2;
 
     }
+    public static int modulusNumber(int num1, int num2) {
+
+
+        return num1 % num2;
+
+    }
     public static void main(String[] args) {
         System.out.println(addNumber(6,2));
         System.out.println(subtractNumber(6,2));
         System.out.println(multiplyNumber(6,2));
         System.out.println(divideNumber(6,2));
+        System.out.println(modulusNumber(6,4));
     }
     }
