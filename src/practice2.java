eecba7f7d66a33dfefa752a7187e4f1135147355
kuwan barnes <Kuwan.barne@gmail.com>
2021-01-31 23:22:47 -0600

    practicing

---
+++ src/practice2.java
@@ -0,0 +1,44 @@
+public class Solution {
+
+    public static void main(String[] args) {
+        /* Enter your code here. Print output to STDOUT. Your class should be named Solution. */
+        System.out.println("Hello, World.");
+        System.out.println("Hello, Java.");
+    }
+}
+import java.util.*;
+
+public class Solution {
+
+    public static void main(String[] args) {
+        Scanner sc=new Scanner(System.in);
+        while(sc.hasNextInt()){
+            int a=sc.nextInt();
+            System.out.println(a);
+        }
+    }
+}
+import java.io.*;
+        import java.math.*;
+        import java.security.*;
+        import java.text.*;
+        import java.util.*;
+        import java.util.concurrent.*;
+        import java.util.regex.*;
+
+public class Solution {
+
+if(n %2=1){
+        System.out.println("Weird")
+    }
+
+    private static final Scanner scanner = new Scanner(System.in);
+
+    public static void main(String[] args) {
+        int N = scanner.nextInt();
+        scanner.skip("(\r\n|[\n\r\u2028\u2029\u0085])?");
+
+        scanner.close();
+    }
+}
+
