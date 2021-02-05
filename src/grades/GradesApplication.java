7e65d62ea822c51d0c2f5e20f9ada2d824c43fbb
kuwan barnes <Kuwan.barne@gmail.com>
2021-02-05 14:56:53 -0600

    finished gradesapp

---
+++ src/grades/GradesApplication.java
@@ -0,0 +1,71 @@
+package grades;
+
+import java.util.HashMap;
+import java.util.Scanner;
+
+public  class GradesApplication {
+    public static void main(String[] args) {
+
+
+        Student newStudent = new Student("kuwan");
+        newStudent.addGrade(80);
+        newStudent.addGrade(90);
+        newStudent.addGrade(60);
+        newStudent.addGrade(10);
+        System.out.println(newStudent.getAverage());
+//        System.out.println(newStudent.getName());
+//        System.out.println(newStudent.grades);
+
+
+        Student newStudent1 = new Student("james");
+        newStudent1.addGrade(89);
+        newStudent1.addGrade(90);
+        newStudent1.addGrade(69);
+        newStudent1.addGrade(70);
+//        System.out.println(newStudent.getAverage());
+//        System.out.println(newStudent.getName());
+        System.out.println(newStudent1);
+
+        Student newStudent2 = new Student("josh");
+        newStudent2.addGrade(80);
+        newStudent2.addGrade(90);
+        newStudent2.addGrade(60);
+        newStudent2.addGrade(90);
+        System.out.println(newStudent.getAverage());
+        System.out.println(newStudent.getName());
+//
+
+        HashMap<String, Student> usernames = new HashMap<>();
+
+        usernames.put("kuwan@gmail.com", newStudent);
+        usernames.put("james@gmail.com", newStudent1);
+        usernames.put("john@gmail.com", newStudent2);
+
+        System.out.println(usernames.keySet());
+//        System.out.println(usernames.size()("kuwan@gmail.com"));
+        System.out.println(newStudent.getGrades());
+
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Welcome");
+        do {
+            System.out.println("Here are the GitHub usernames of our students: \n" + usernames.keySet());
+            System.out.println("enter the username of your choice:");
+            String studentUsername = scanner.nextLine().trim();
+            if (usernames.get(studentUsername) == null) {
+                System.out.println("Did not match any student");
+            } else {
+                System.out.printf("Name: %s - Github Username: %s\n", usernames.get(studentUsername).getName(), studentUsername);
+                System.out.printf("Current Average: %.2f\n", usernames.get(studentUsername).getAverage());
+                System.out.printf("Would you like to see another student ? type \"no\" to exit\n");
+                studentUsername = scanner.nextLine();
+                if (studentUsername.equalsIgnoreCase("no")) {
+                    break;
+                }
+//            System.out.println(usernames.get(studentUsername).getGrades());
+            }
+        }
+            while (true) ;
+
+
+    }
+}
--- src/grades/Student.java
+++ src/grades/Student.java
@@ -1,13 +1,17 @@
 package grades;
 
 import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.HashMap;
 
 public class Student {
     private String name;
 private ArrayList<Integer> grades;
-    public int total=0;
 
 
+    public ArrayList getGrades() {
+        return this.grades;
+    }
 
 
 
@@ -31,24 +35,60 @@ private ArrayList<Integer> grades;
 
     }
         public  double getAverage(){
+             double total=0;
 //            for (Integer grade : grades) {
 //                total = total + grade;
 //            }
             for(int i=0; i<grades.size(); i++){
-                total = total + grades.get(i);
+                total += this.grades.get(i);
             }
-            return (float) (total/grades.size());
+            return (total/this.grades.size());
 
         }
 
+
     public static void main(String[] args) {
-Student mygrades = new Student("kuwan");
-        mygrades.addGrade(80);
-        mygrades.addGrade(90);
-        mygrades.addGrade(60);
-        mygrades.addGrade(0);
-        System.out.println(mygrades.getAverage());
-        System.out.println(mygrades.getName());
+Student newStudent = new Student("kuwan");
+        newStudent.addGrade(80);
+        newStudent.addGrade(90);
+        newStudent.addGrade(60);
+        newStudent.addGrade(0);
+//        System.out.println(newStudent.getAverage());
+//        System.out.println(newStudent.getName());
+//        System.out.println(newStudent.grades);
+
+
+        Student newStudent1 = new Student("james");
+        newStudent.addGrade(89);
+        newStudent.addGrade(90);
+        newStudent.addGrade(69);
+        newStudent.addGrade(70);
+//        System.out.println(newStudent.getAverage());
+//        System.out.println(newStudent.getName());
+//        System.out.println(newStudent.grades);
+
+        Student newStudent2 = new Student("josh");
+        newStudent.addGrade(80);
+        newStudent.addGrade(90);
+        newStudent.addGrade(60);
+        newStudent.addGrade(90);
+        System.out.println(newStudent.getAverage());
+        System.out.println(newStudent.getName());
+        System.out.println(newStudent.grades);
+
+//        HashMap<String, Student> usernames = new HashMap<>();
+//        usernames.put("kuwan@gmail.com",newStudent);
+//        usernames.put("james@gmail.com",newStudent1);
+//        usernames.put("john@gmail.com",newStudent2);
+//        System.out.println(usernames.get("kuwan@gmail.com"));
+//        String[] keys = usernames.keySet();
+//        for(String key: usernames.keySet()){
+//            Student thisStudent = usernames.get(key);
+////            System.out.println(thisStudent.getName());
+//        }
+//        System.out.println(usernames.keySet());
+//        System.out.println(keys);
+
 
     }
 }
