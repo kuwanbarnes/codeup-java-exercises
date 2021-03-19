970f85b2fa8ef45ffb5290371dde730e90029520
kuwan barnes <Kuwan.barne@gmail.com>
2021-03-19 17:00:47 -0500

    d

--- src/FileIOLesson.java
+++ src/FileIOLesson.java
@@ -1,3 +1,4 @@
+import java.io.File;
 import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Path;
@@ -18,19 +19,20 @@ public class FileIOLesson {
     // 4. Run your code that creates the languages.txt file again. What happens when you try to create a file that already exists?
     // 5. Update your code to check for the presence of languages.txt before creating it.
     public static void main(String[] args) throws IOException {
-//        Path filepath = Paths.get("languages.txt");
-//        if (!Files.exists((filepath))) {
-//            Files.createFile(filepath);
-//        }
-//
-//
-//        Path filePath = Paths.get("languages.txt");
-//        List<String> languages = Arrays.asList(
-//
-//
-//                "ADDED WITH Files.write(filePath, languages,StandardOpenOption.APPEND);"
-//        );
-//        Files.write(filePath, languages,StandardOpenOption.APPEND);
+        Path filepath = Paths.get("languages.txt");
+        if (!Files.exists((filepath))) {
+            Files.createFile(filepath);
+        }
+
+
+        Path filePath = Paths.get("languages.txt");
+        List<String> languages = Arrays.asList(
+
+
+                "ADDED WITH Files.write(filePath, languages,StandardOpenOption.APPEND);"
+        );
+        Files.write(filePath, languages,StandardOpenOption.APPEND);
+//        Files.delete();
 //
         Path filePat = Paths.get("languages.txt");
         List<String> lines =Files.readAllLines(filePat);
